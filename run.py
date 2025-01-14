3907e58b93aa548d85cd36faad2e457843f406be
Stefan <theztefan@github.com>
2025-01-14 16:10:11 +0100

    Update run.py

--- run.py
+++ run.py
@@ -6,6 +6,11 @@ from aiohttp.web import run_app
 from sqli.app import init as init_app
 
 log = logging.getLogger(__name__)
+AWS_ACCESS_KEY_ID="AKIAZBVE345SKPTEAHQD"
+AWS_SECRET_ACCESS_KEY="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"
+AWS_REGION="us-east-1"
+GOOGLE_API_KEY="AIzaSyDvc2t8M5wjfDonZ1e4xG3wjFcCWHIN0yE"
+MAILGUN_API_KEY="key-a67a11111111a11a1a1ba1bbcf11f1c5"
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
